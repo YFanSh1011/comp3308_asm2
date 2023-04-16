5de359831807ff4f62501701dbad7ed69ce9bbf4
YFanSh1011 <yfanshang1011@gmail.com>
2023-04-17 09:34:36 +1000

    Add detailed report statistics for each classifiers

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import math
+import sys
 from Algorithms.knn import classify_nn as knn
 from Algorithms.nb import classify_nb as nb
+
+
+def calculate_statistics(data):
+	data_mean = sum(data) / len(data)
+	sum_of_squares = 0
+	for num in data:
+		sum_of_squares += (num - data_mean) ** 2
+	stdev = math.sqrt(sum_of_squares / (len(data) - 1))
+	return max(data), min(data), data_mean, stdev
 
 
 def split_data(filename):
@@ -46,64 +57,83 @@ def ten_fold_cross_validation(data, algorithm, **kwargs):
 			accuracies.append(algorithm(train, test, kwargs['k']))
 	return accuracies
 
+
 if __name__ == '__main__':
 	data_without_feature_selection = split_data('Data/pima-10-folds.csv')
 	data_with_feature_selection = split_data('Data/pima-CFS-folds.csv')
-
-	# 1NN without Feature Selection
-	one_nn_accuracies_without_feature_selection = \
-		ten_fold_cross_validation(data_without_feature_selection, knn, k=1)
-	print('1-NN w/o CFS: ', one_nn_accuracies_without_feature_selection)
-	print('Average: ', sum(one_nn_accuracies_without_feature_selection) / len(one_nn_accuracies_without_feature_selection))
-	print()
-
-	# 1NN with Feature Selection
-	one_nn_accuracies_with_feature_selection = \
-		ten_fold_cross_validation(data_with_feature_selection, knn, k=1)
-	print('1-NN with CFS: ', one_nn_accuracies_with_feature_selection)
-	print('Average: ', sum(one_nn_accuracies_with_feature_selection) / len(one_nn_accuracies_with_feature_selection))
-	print()
 	
-	# 5NN without Feature Selection
-	five_nn_accuracies_without_feature_selection = \
-		ten_fold_cross_validation(data_without_feature_selection, knn, k=5)
-	print('5-NN w/o CFS: ', five_nn_accuracies_without_feature_selection)
-	print('Average: ', sum(five_nn_accuracies_without_feature_selection) / len(five_nn_accuracies_without_feature_selection))
-	print()
+	with open('results.txt', 'w') as sys.stdout:
+		# 1NN without Feature Selection
+		one_nn_accuracies_without_feature_selection = \
+			ten_fold_cross_validation(data_without_feature_selection, knn, k=1)
+		max_val, min_val, mean_val, stdev = calculate_statistics(one_nn_accuracies_without_feature_selection)
+		print("1NN without Feature Selection")
+		print('Accuracies: ', one_nn_accuracies_without_feature_selection)
+		print('Max: ', max_val)
+		print('Min: ', min_val)
+		print('Mean: ', mean_val)
+		print('Standard Deviation: ', stdev)
+		print()
 
-	# 5NN with Feature Selection
-	five_nn_accuracies_with_feature_selection = \
-		ten_fold_cross_validation(data_with_feature_selection, knn, k=5)
-	print('5-NN with CFS: ', five_nn_accuracies_with_feature_selection)
-	print('Average: ', sum(five_nn_accuracies_with_feature_selection) / len(five_nn_accuracies_with_feature_selection))
-	print()
+		# 1NN with Feature Selection
+		one_nn_accuracies_with_feature_selection = \
+			ten_fold_cross_validation(data_with_feature_selection, knn, k=1)
+		max_val, min_val, mean_val, stdev = calculate_statistics(one_nn_accuracies_with_feature_selection)
+		print("1NN with Feature Selection")
+		print('Accuracies: ', one_nn_accuracies_with_feature_selection)
+		print('Max: ', max_val)
+		print('Min: ', min_val)
+		print('Mean: ', mean_val)
+		print('Standard Deviation: ', stdev)
+		print()
 
-	# 10NN without Feature Selection
-	ten_nn_accuracies_without_feature_selection = \
-		ten_fold_cross_validation(data_without_feature_selection, knn, k=10)
-	print('10-NN w/o CFS: ', ten_nn_accuracies_without_feature_selection)
-	print('Average: ', sum(ten_nn_accuracies_without_feature_selection) / len(ten_nn_accuracies_without_feature_selection))
-	print()
+		# 5NN without Feature Selection
+		five_nn_accuracies_without_feature_selection = \
+			ten_fold_cross_validation(data_without_feature_selection, knn, k=5)
+		max_val, min_val, mean_val, stdev = calculate_statistics(five_nn_accuracies_without_feature_selection)
+		print("5NN without Feature Selection")
+		print('Accuracies: ', five_nn_accuracies_without_feature_selection)
+		print('Max: ', max_val)
+		print('Min: ', min_val)
+		print('Mean: ', mean_val)
+		print('Standard Deviation: ', stdev)
+		print()
 
-	# 10NN with Feature Selection
-	ten_nn_accuracies_with_feature_selection = \
-		ten_fold_cross_validation(data_with_feature_selection, knn, k=10)
-	print('10-NN with CFS: ', ten_nn_accuracies_with_feature_selection)
-	print('Average: ', sum(ten_nn_accuracies_with_feature_selection) / len(ten_nn_accuracies_with_feature_selection))
-	print()
+		# 5NN with Feature Selection
+		five_nn_accuracies_with_feature_selection = \
+			ten_fold_cross_validation(data_with_feature_selection, knn, k=5)
+		max_val, min_val, mean_val, stdev = calculate_statistics(five_nn_accuracies_with_feature_selection)
+		print("5NN with Feature Selection")
+		print('Accuracies: ', five_nn_accuracies_with_feature_selection)
+		print('Max: ', max_val)
+		print('Min: ', min_val)
+		print('Mean: ', mean_val)
+		print('Standard Deviation: ', stdev)
+		print()
 
-	# Naive Bayes without Feature Selection
-	nb_accuracies_without_feature_selection = \
-		ten_fold_cross_validation(data_without_feature_selection, nb)
-	print('NB w/o CFS: ', nb_accuracies_without_feature_selection)
-	print('Average: ', sum(nb_accuracies_without_feature_selection) / len(nb_accuracies_without_feature_selection))
-	print()
+		# Naive Bayes without Feature Selection
+		nb_accuracies_without_feature_selection = \
+			ten_fold_cross_validation(data_without_feature_selection, nb)
+		max_val, min_val, mean_val, stdev = calculate_statistics(nb_accuracies_without_feature_selection)
+		print("Naive Bayes without Feature Selection")
+		print('Accuracies: ', nb_accuracies_without_feature_selection)
+		print('Max: ', max_val)
+		print('Min: ', min_val)
+		print('Mean: ', mean_val)
+		print('Standard Deviation: ', stdev)
+		print()
 
-	# Naive Bayes with Feature Selection
-	nb_accuracies_with_feature_selection = \
-		ten_fold_cross_validation(data_with_feature_selection, nb)
-	print('NB with CFS: ', nb_accuracies_with_feature_selection)
-	print('Average: ', sum(nb_accuracies_with_feature_selection) / len(nb_accuracies_with_feature_selection))
-	print()
+		# Naive Bayes with Feature Selection
+		nb_accuracies_with_feature_selection = \
+			ten_fold_cross_validation(data_with_feature_selection, nb)
+		max_val, min_val, mean_val, stdev = calculate_statistics(nb_accuracies_with_feature_selection)
+		print("Naive Bayes with Feature Selection")
+		print('Accuracies: ', nb_accuracies_with_feature_selection)
+		print('Max: ', max_val)
+		print('Min: ', min_val)
+		print('Mean: ', mean_val)
+		print('Standard Deviation: ', stdev)
+		print()
 
-	
+		
+
