94202aef9ed5b74266b969f03a9ae849271e6bf3
Yuefan Shang <yfanshang1011@gmail.com>
2023-04-25 15:00:07 +1000

    Implement calculation for recall, precision, and F1 measure

--- compare_classifier.py
+++ compare_classifier.py
@@ -27,6 +27,16 @@ def calculate_statistical_significance(data_set1, data_set2, confidence_level=0.
 	return (z_score_upper >= 0 and  0 >= z_score_lower)
 
 
+def confusion_matrix_statistics(cm):
+	# Calculate precision
+	precision = round(cm["True Positive"] / (cm["True Positive"] + cm["False Positive"]), 4)
+	# Calculate recall
+	recall = round(cm["True Positive"] / (cm["True Positive"] + cm["False Negative"]), 4)
+	# Calculate F1 score
+	f1_score = round(2 * ((precision * recall) / (precision + recall)), 4)
+	return precision, recall, f1_score
+
+
 if __name__ == "__main__":
 	# Load data from json file
 	try:
@@ -36,13 +46,19 @@ if __name__ == "__main__":
 		exit()
 	
 	one_nn_wo_fs = data_dict['1NN without Feature Selection']["Accuracies"]
+	one_nn_wo_fs_cm = data_dict['1NN without Feature Selection']["Confusion Matrix"]
 	one_nn_w_fs = data_dict['1NN with Feature Selection']["Accuracies"]
+	one_nn_w_fs_cm = data_dict['1NN with Feature Selection']["Confusion Matrix"]
 
 	five_nn_wo_fs = data_dict['5NN without Feature Selection']["Accuracies"]
+	five_nn_wo_fs_cm = data_dict['5NN without Feature Selection']["Confusion Matrix"]
 	five_nn_w_fs = data_dict['5NN with Feature Selection']["Accuracies"]
+	five_nn_w_fs_cm = data_dict['5NN with Feature Selection']["Confusion Matrix"]
 	
 	nb_wo_fs = data_dict['Naive Bayes without Feature Selection']["Accuracies"]
+	nb_wo_fs_cm = data_dict['Naive Bayes without Feature Selection']["Confusion Matrix"]
 	nb_w_fs = data_dict['Naive Bayes with Feature Selection']["Accuracies"]
+	nb_w_fs_cm = data_dict['Naive Bayes with Feature Selection']["Confusion Matrix"]
 	
 	with open("Results/comparison_result.txt", "w") as sys.stdout:
 		print("------------------------")
@@ -51,16 +67,22 @@ if __name__ == "__main__":
 		# Compare the result with or without feature selection for 1NN
 		print('1NN without Feature Selection vs 1NN with Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(one_nn_wo_fs, one_nn_w_fs) else "IS NOT"))
+		print("1NN WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(one_nn_wo_fs_cm)))
+		print("1NN W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(one_nn_w_fs_cm)))
 		print()
 
 		# Compare the result with or without feature selection for 5NN
 		print('5NN without Feature Selection vs 5NN with Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(five_nn_wo_fs, five_nn_w_fs) else "IS NOT"))
+		print("5NN WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(five_nn_wo_fs_cm)))
+		print("5NN W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(five_nn_w_fs_cm)))
 		print()
 
 		# Compare the result with or without feature selection for Naive Bayes
 		print('Naive Bayes without Feature Selection vs Naive Bayes with Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(nb_wo_fs, nb_w_fs) else "IS NOT"))
+		print("NB WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(nb_wo_fs_cm)))
+		print("NB W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(nb_w_fs_cm)))
 		print()
 		print()
 
@@ -70,16 +92,22 @@ if __name__ == "__main__":
 		# Compare the result of 1NN with 5NN
 		print('1NN without Feature Selection vs 5NN without Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(one_nn_wo_fs, five_nn_wo_fs) else "IS NOT"))
+		print("1NN WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(one_nn_wo_fs_cm)))
+		print("5NN WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(five_nn_wo_fs_cm)))
 		print()
 
 		# Compare the result of 1NN with Naive Bayes
 		print('1NN without Feature Selection vs Naive Bayes without Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(one_nn_wo_fs, nb_wo_fs) else "IS NOT"))
+		print("1NN WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(one_nn_wo_fs_cm)))
+		print("NB WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(nb_wo_fs_cm)))
 		print()
 
 		# Compare the result of 5NN with Naive Bayes
 		print('5NN without Feature Selection vs Naive Bayes without Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(five_nn_wo_fs, nb_wo_fs) else "IS NOT"))
+		print("5NN WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(five_nn_wo_fs_cm)))
+		print("NB WO FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(nb_wo_fs_cm)))
 		print()
 		print()
 		
@@ -89,14 +117,20 @@ if __name__ == "__main__":
 		# Compare the result of 1NN with 5NN
 		print('1NN with Feature Selection vs 5NN with Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(one_nn_w_fs, five_nn_w_fs) else "IS NOT"))
+		print("1NN W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(one_nn_w_fs_cm)))
+		print("5NN W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(five_nn_w_fs_cm)))
 		print()
 
 		# Compare the result of 1NN with Naive Bayes
 		print('1NN with Feature Selection vs Naive Bayes with Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(one_nn_w_fs, nb_w_fs) else "IS NOT"))
+		print("1NN W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(one_nn_w_fs_cm)))
+		print("NB W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(nb_w_fs_cm)))
 		print()
 
 		# Compare the result of 5NN with Naive Bayes
 		print('5NN with Feature Selection vs Naive Bayes with Feature Selection:')
 		print("There {} a statistical significance between the two algorithms.".format("IS" if calculate_statistical_significance(five_nn_w_fs, nb_w_fs) else "IS NOT"))
+		print("5NN W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(five_nn_w_fs_cm)))
+		print("NB W FS: Precision: {}, Recall: {}, F1 Score: {}".format(*confusion_matrix_statistics(nb_w_fs_cm)))
 		print()
